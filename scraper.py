c6781d9c88681a67df48dc07fbad3aea50b2ce70
zubbyik <zubbyik@gmail.com>
2018-03-14 04:51:42 +0000

    update of few packages

---
+++ scraper.py
@@ -0,0 +1,13 @@
+from scrapely import Scraper
+from wtforms import Form
+
+s = Scraper()
+
+
+#  Collect details from the form input as a json
+class SearchForms(Form):
+    pass
+#  Collect all the links that would be scraped
+#  List the items that needs to be scraped
+#  Train the scrapers with the data on the urls
+#
